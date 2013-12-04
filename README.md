a9004cb33e08948093adfe5d4d7cb0bb2630ab6b
Tim Thomas <tim@buyplaytix.com>
2013-12-04 10:37:32 -0600

    readying for upload

---
+++ README.md
@@ -0,0 +1,20 @@
+smdld
+=====
+
+smdld is a simple PHP command line utility to download and backup smugmug galleries. 
+
+It currently operates anonymously so it can only download public images.
+
+Getting Started
+---------------
+
+  * Copy lib/key.php.sample to lib/key.php
+  * [Apply for an API key](http://www.smugmug.com/hack/apikeys) from SmugMug
+  * Put your API key, nickname and application name in lib/key.php
+  * Download [composer](http://getcomposer.org/) and run composer install from the root directory
+  
+That's it! Run smdld and you'll be prompted with the gallery to download. Downloading a gallery
+with sub-galleries will download all sub-galleries. 
+
+smdld uses [phpSmug](http://phpsmug.com/docs) to do the API heavy lifting.
+
--- smdld.php
+++ smdld.php
@@ -1,85 +1,68 @@
 #!/usr/bin/env php
 <?php
-require_once("lib/key.php");
-require_once( "lib/phpSmug.php" );
-require_once("vendor/autoload.php");
+require_once ("lib/key.php");
+require_once ("lib/phpSmug.php");
+require_once ("vendor/autoload.php");
 
 try {
-  // $API_KEY  and $APP_NAME are globals from key.php
-	$f = new phpSmug( "APIKey=" . $API_KEY, "AppName=" . $APP_NAME );
-	// Login Anonymously
-	$f->login();	
-	// Get list of public albums
-	$albums = $f->albums_get( 'NickName=thomasfamily' );	
-
-  $categories = array();
-  foreach($albums as $album) {
-    $categories[$album["Category"]["id"]] = $album["Category"]["Name"];
-    if(array_key_exists('SubCategory', $album)) {
-      $categories[$album["SubCategory"]["id"]] = $album["Category"]["Name"] . "/" . $album["SubCategory"]["Name"];
-      $categories[$album["id"]] = $album["Category"]["Name"] . "/" . $album["SubCategory"]["Name"] . "/" . $album["Title"];
-
-    } else {
-      $categories[$album["id"]] = $album["Category"]["Name"] . "/" . $album["Title"];
+    // $API_KEY and $APP_NAME are globals from key.php
+    $f = new phpSmug("APIKey=" . $API_KEY, "AppName=" . $APP_NAME);
+    // Login Anonymously
+    $f->login();
+    // Get list of public albums
+    $albums = $f->albums_get('NickName=' . $NICKNAME);
+    
+    $categories = array();
+    foreach ($albums as $album) {
+        $categories[$album["Category"]["id"]] = $album["Category"]["Name"];
+        if (array_key_exists('SubCategory', $album)) {
+            $categories[$album["SubCategory"]["id"]] = $album["Category"]["Name"] . "/" . $album["SubCategory"]["Name"];
+            $categories[$album["id"]] = $album["Category"]["Name"] . "/" . $album["SubCategory"]["Name"] . "/" . $album["Title"];
+        } else {
+            $categories[$album["id"]] = $album["Category"]["Name"] . "/" . $album["Title"];
+        }
     }
-  }
-  natsort($categories);
-  foreach($categories as $category_id => $category_name) {
-    \cli\line( $category_id. ") " . $category_name);
-  }
-  $id = \cli\prompt("Please enter a gallery id");
-
-  $key = NULL;
-  $dnld = array();
-  foreach($albums as $album) {
-    if($album["id"] == $id) {
-      $dnld[$album["id"]] = $album["Key"];
+    natsort($categories);
+    foreach ($categories as $category_id => $category_name) {
+        cli\line($category_id . ") " . $category_name);
     }
-    elseif(array_key_exists('Category', $album) && $album["Category"]["id"] == $id) {
-      $dnld[$album["id"]] = $album["Key"];
+    $id = \cli\prompt("Please enter a gallery id");
+    
+    $key = NULL;
+    $dnld = array();
+    foreach ($albums as $album) {
+        if ($album["id"] == $id) {
+            $dnld[$album["id"]] = $album["Key"];
+        } elseif (array_key_exists('Category', $album) && $album["Category"]["id"] == $id) {
+            $dnld[$album["id"]] = $album["Key"];
+        } elseif (array_key_exists('SubCategory', $album) && $album["SubCategory"]["id"] == $id) {
+            $dnld[$album["id"]] = $album["Key"];
+        }
     }
-    elseif(array_key_exists('SubCategory', $album) && $album["SubCategory"]["id"] == $id) {
-      $dnld[$album["id"]] = $album["Key"];
+    
+    foreach ($dnld as $id => $key) {
+        $images = $f->images_get("AlbumID={$id}", "AlbumKey={$key}", "Heavy=1");
+        if (! file_exists("Download/" . $categories[$id])) {
+            mkdir("Download/" . $categories[$id], 0777, true);
+        }
+        $count = 0;
+        foreach ($images["Images"] as $image) {
+            $path = "Download/" . $categories[$id] . "/" . $image["FileName"];
+            if (file_exists($path) && filesize($path) == $image["Size"]) {
+                cli\line("Skipping " . $image["FileName"]);
+                continue;
+            }
+            cli\line("Downloading " . $image["FileName"]);
+            
+            if (array_key_exists("Video1280URL", $image)) {
+                file_put_contents($path, file_get_contents($image["Video1280URL"]));
+            } else {
+                file_put_contents($path, file_get_contents($image["OriginalURL"]));
+            }
+            $count ++;
+        }
     }
-  }
- 
-  foreach($dnld as $id => $key) { 
-  $images = $f->images_get( "AlbumID={$id}", "AlbumKey={$key}", "Heavy=1" );
-  if(!file_exists("Download/" . $categories[$id])) { 
-    mkdir("Download/" . $categories[$id], 0777, true);
-  }
-  $count = 0;
-  foreach($images["Images"] as $image) {
-    $path = "Download/" . $categories[$id] . "/" . $image["FileName"];
-    if(file_exists($path) && filesize($path) == $image["Size"]) {
-      \cli\line("Skipping " . $image["FileName"]);
-      continue;
-    }
-    \cli\line("Downloading " . $image["FileName"]);
-
-    if(array_key_exists("Video1280URL", $image)) {
-      file_put_contents($path, file_get_contents($image["Video1280URL"]));
-
-    }
-    else {
-      file_put_contents($path, file_get_contents($image["OriginalURL"]));
-    }
-    $count++;
-  }
-  }
-  \cli\line("Downloaded $count images");
-//  print_r($images);
-  /*
-	// Get list of public images and other useful information
-	$images = $f->images_get( "AlbumID={$albums['0']['id']}", "AlbumKey={$albums['0']['Key']}", "Heavy=1" );
-	$images = ( $f->APIVer == "1.2.2" ) ? $images['Images'] : $images;
-	// Display the thumbnails and link to the medium image for each image
-	foreach ( $images as $image ) {
-		echo '<a href="'.$image['MediumURL'].'"><img src="'.$image['TinyURL'].'" title="'.$image['Caption'].'" alt="'.$image['id'].'" /></a>';
-	}
-  */
+    cli\line("Downloaded $count images");
+} catch (Exception $e) {
+    echo "{$e->getMessage()} (Error Code: {$e->getCode()})";
 }
-catch ( Exception $e ) {
-	echo "{$e->getMessage()} (Error Code: {$e->getCode()})";
-}
-?>
